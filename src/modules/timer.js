7b551fdc64682b79861f967f219183460bacd764
Vladimir-cloudj <wizart88@gmail.com>
2026-04-08 21:48:20 +0400

    feat: lesson19

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -8,6 +8,14 @@ const timer = (deadline) => {
         let dateStop = new Date(deadline).getTime();
         let dateNow = new Date().getTime();
         let timeRemaining = (dateStop - dateNow) / 1000;
+        if (timeRemaining <= 0) {
+          return {
+            timeRemaining,
+            hours: 0, 
+            minutes: 0,
+            seconds: 0
+          }
+        }
         let days = Math.floor((timeRemaining / 60 / 60 / 24) );
         let hours = Math.floor((timeRemaining / 3600) % 24);
         let minutes = Math.floor((timeRemaining / 60) % 60);
@@ -23,16 +31,31 @@ const timer = (deadline) => {
 
     const updateClock = () => {
       let getTime = getTimeRemaining();
-      // вывести количество дней перед остальными числами
-      timerHours.textContent = getTime.hours;
-      timerMinutes.textContent = getTime.minutes;
-      timerSeconds.textContent = getTime.seconds;
-    
-      if (getTime.timeRemaining > 0) {
-        setTimeout(updateClock, 1000);
+      if (getTime.timeRemaining <= 0) {
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+        return
       }
+      // вывести количество дней перед остальными числами
+      timerHours.textContent = addZero(getTime.hours);
+      timerMinutes.textContent = addZero(getTime.minutes);
+      timerSeconds.textContent = addZero(getTime.seconds);
+    
+      // if (getTime.timeRemaining > 0) {
+        // setTimeout(updateClock, 1000);
+      // }
     }
+
+    function addZero(num) {
+      if (num < 10) {
+        return '0' + num;
+      }
+      return num;
+    }
+
     updateClock();
+    setInterval(updateClock, 1000)
 }
 
 export default timer
