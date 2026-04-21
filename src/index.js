fecd7fc6303b7bd7aaed95c48874c97c9960b537
Vladimir-cloudj <73839515+Vladimir-cloudj@users.noreply.github.com>
2026-04-21 14:39:21 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import menu from './modules/menu.js'
 import timer from './modules/timer.js'
 import modal from './modules/modal.js'
+import smoothScroll from './modules/smooth-scroll';
 
 timer("29 june 2026");
 
 menu();
 
 modal();
+
+smoothScroll();
