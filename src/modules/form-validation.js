22e9cf12e1be4dfa0899a457a58adedaee1478af
Vladimir-cloudj <73839515+Vladimir-cloudj@users.noreply.github.com>
2026-05-12 15:00:04 +0400

    Update form-validation using BLUR

--- src/modules/form-validation.js
+++ src/modules/form-validation.js
@@ -1,6 +1,22 @@
 const formValidation = () => {
-  const telInputs = document.querySelectorAll('input[type="tel"]');
 
+  function cleanDuplicates(str) {
+    return str.replace(/\s{2,}/g, " ").replace(/-{2,}/g, "-");
+  }
+
+  function cleanEdges(str) {
+    return str.replace(/^[\s-]+|[\s-]+$/g, "");
+  }
+
+  function capitalizeWords(str) {
+    return str.replace(/\b\w+/g, (word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    });
+  }
+
+  // Валидация через INPUT
+
+  const telInputs = document.querySelectorAll('input[type="tel"]');
   telInputs.forEach((input) => {
     input.addEventListener("input", function () {
       this.value = this.value.replace(/[^0-9()+\-\s]/g, "");
@@ -8,7 +24,6 @@ const formValidation = () => {
   });
 
   const emailInputs = document.querySelectorAll('input[type="email"]');
-
   emailInputs.forEach((input) => {
     input.addEventListener("input", function () {
       this.value = this.value.replace(/[^a-zA-Z0-9@\-_.!~*'']/g, "");
@@ -18,10 +33,39 @@ const formValidation = () => {
   const messageInputs = document.querySelectorAll(
     'input[placeholder="Ваше сообщение"], textarea[placeholder="Ваше сообщение"]',
   );
-
   messageInputs.forEach((input) => {
     input.addEventListener("input", function () {
       this.value = this.value.replace(/[^а-яА-ЯёЁ\-\s]/g, "");
+    });
+  });
+
+  // Валидация через BLUR
+
+  telInputs.forEach((input) => {
+    input.addEventListener("blur", function () {
+      let value = this.value;
+      value = cleanDuplicates(value);
+      value = cleanEdges(value);
+      this.value = value;
+    });
+  });
+
+  emailInputs.forEach((input) => {
+    input.addEventListener("blur", function () {
+      let value = this.value;
+      value = cleanDuplicates(value);
+      value = cleanEdges(value);
+      this.value = value;
+    });
+  });
+
+  messageInputs.forEach((input) => {
+    input.addEventListener("blur", function () {
+      let value = this.value;
+      value = cleanDuplicates(value);
+      value = cleanEdges(value);
+      value = capitalizeWords(value);
+      this.value = value;
     });
   });
 };
