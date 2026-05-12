ee6224da00e28366c603f8e480aca01687253379
Vladimir-cloudj <73839515+Vladimir-cloudj@users.noreply.github.com>
2026-05-12 17:55:18 +0400

    Update menu - change togglemenu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,21 +1,33 @@
 const menu = () => {
-    const menuBtn = document.querySelector('.menu');
-    const menuElement = document.querySelector('menu');
-    
-    const handleMenu = () => {
-        menuElement.classList.toggle('active-menu');
-    };
-    
-    menuBtn.addEventListener('click', handleMenu);
-    
-    menuElement.addEventListener('click', (event) => {
-        if (event.target.closest('.close-btn')) {
-            handleMenu();
-        }
-        else if (event.target.closest('li > li > a')) {
-            handleMenu();
-        }
-    });
+  const menuBtn = document.querySelector(".menu");
+  const menuElement = document.querySelector("menu");
+  const closeBtn = menuElement.querySelector(".close-btn");
+
+  const toggleMenu = () => {
+    menuElement.classList.toggle("active-menu");
+  };
+  menuElement.addEventListener("click", (event) => {
+    if (event.target.closest(".close-btn")) {
+      toggleMenu();
+    }
+    else if (event.target.closest("li > li > a")) {
+      toggleMenu();
+    }
+  });
+  menuBtn.addEventListener("click", toggleMenu);
+
+  document.addEventListener("click", (event) => {
+    const isClickInsideMenu = menuElement.contains(event.target);
+    const isClickOnMenuBtn = menuBtn.contains(event.target);
+
+    if (
+      menuElement.classList.contains("active-menu") &&
+      !isClickInsideMenu &&
+      !isClickOnMenuBtn
+    ) {
+      toggleMenu();
+    }
+  });
 };
 
 export default menu;
